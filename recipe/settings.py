2b32e7b5dff3cb12f59841f90180bb0ae539aa73
Ubuntu <ubuntu@ip-172-31-46-8.us-east-2.compute.internal>
2023-02-27 18:42:05 +0000

    recollect static file

--- recipe/settings.py
+++ recipe/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 from decouple import config
 import platform
 import boto3
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -160,7 +161,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'recipe_app', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
